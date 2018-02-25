27e0ebcc5644044350a9893f784a61044c9c65a0
David Narvaez <dnarvaez27@outlook.com>
2018-02-25 17:47:07 -0500

    Radial Tree SVG

---
+++ src/tree/radial/README.md
@@ -0,0 +1,81 @@
+# Radial Tree
+
+This library makes a radial representation of a tree data structure.
+
+### Before you Start
+In order to make use of this library, you must include [tree.js](https://github.com/dnarvaez27/JavaScript-Graphics/blob/master/src/datastructures/tree/tree.js)
+and [svg.js*](https://github.com/dnarvaez27/JavaScript-Graphics/blob/master/src/tree/radial/svg.min.js)
+
+## Usage
+1. You must have a ```div``` where the SVG will be contained
+2. Configurate the library to your needs through the ```radial_config``` JS object, with the following options:  
+    * ```container_id```: The ```id``` of the div which will contain the SVG radial tree
+    * ```tree```: The Tree data structure configured with the information required. See more information below
+    * ```clickListener```: An event handler when a node of the radial tree is clicked. The parameter associated to the handler is the data defined in the Tree node
+3. Personalize the radial tree through the ```radial_attrs``` JS object with the following options:  
+    * ```inc```: The increment in pixels of the different levels of the radial tree. By default, this attribute will be calculated to fit the container **height**
+    * ```size```: The size of each node in the radial tree. By default it is configured to 25px
+    * ```padding```: The padding between the nodes of the tree. By default it is configured to 10px
+    * ```style```: Configures the style of the visual elements of the radial tree:
+        * ```line```: Sets the SVG style for the lines between two nodes of the radial tree.
+        * ```ring```: Sets the SVG style for the ring levels of the radial tree.
+        * ```node```: Sets the different configurations for the nodes of the radial tree:
+            * ```bg```: Sets the background color of every default node in the radial tree
+            * ```fg```: Sets the foreground color of every default node in the radial tree
+            * ```anim```: Sets the scale for the nodes when the mouse is over the nodes of the radial tree.
+                          This **object** has two properties: 
+                * ```scale```: Sets the scale of the whole node of the radial tree
+                * ```textScale```: Sets the scale of the content of the radial tree's node
+
+## Tree Data Structure
+The Tree Data Structure, provides an easy way of adding nodes to the tree through the function ```add(node, parent)```
+where the ```node``` parameter states for the node to be added, and the ```parent``` states for the ```id``` of the parent of the new node.  
+Each node can contain the following attributes:
+* ```id```: Id of the node
+* ```data```: The data associated to the node, this can contain more information to display in the radial tree:
+    * ```style```: This JS object, contains the data needed to set de ```bg``` (Background) and the ```fg``` (Foreground) of the node.
+    * ```display```: This object contains the data that will be shown in the radial tree. This object has four fields:
+        * ```normal```: The data that will be shown in each node.
+        * ```hover```: The data that will be shown in the node when the user hovers the mouse
+        * ```normal_svg```: **Experimental.** The data in SVG format that will be shown in each node.
+        * ```hover_svg```: **Experimental.** The data in SVG format that will be shown in the node when the user hovers the mouse.
+
+## Code Sample
+
+``` JavaScript
+let ntree = new Tree();
+ntree.add( {
+               id: 0,
+               data: {
+                   style: {
+                       bg: '#009696',
+                       fg: '#f2f7ff'
+                   },
+                   display: {
+                       normal: 'Me',
+                       hover: 'David\n Narvaez'
+                   }
+               }
+           }, undefined ); // The first element does not have a parent
+ntree.add( { id: 1, data: 1 }, 0 );
+ntree.add( { id: 2, data: 2 }, 1 );
+ntree.add( { id: 3, data: 3 }, 1 );
+
+
+radial_config.tree = ntree;
+radial_config.container_id = 'radial'
+radial_config.clickListener = function ( elem ) {
+    console.log( elem );
+};
+radial_attrs.inc = 80;
+radial_attrs.size = 30;
+radial_attrs.padding = 40;
+radial_attrs.style.node.anim.scale = 2.5;
+```
+And for the HTML file, the container with its ```id```
+``` HTML
+<div id="radial"></div> 
+```
+
+## Result Sample
+![](https://github.com/dnarvaez27/JavaScript-Graphics/blob/master/imgs/RadialTree.JPG)
---
+++ src/tree/radial/radial.js
@@ -0,0 +1,235 @@
+/**
+ * radial.js
+ * Radial Tree Grapher using SVG.js http://svgjs.com/
+ * tree.js is required. (See in datastructures: https://github.com/dnarvaez27/JavaScript-Graphics)
+ *
+ * @license Apache Licence, https://www.apache.org/licenses/LICENSE-2.0
+ * @version 1.0
+ * @author  David Narvaez Guerrero
+ * @updated 2018-02-25
+ * @link    https://github.com/dnarvaez27/JavaScript-Graphics
+ *
+ */
+
+'use strict';
+
+let radial_attrs = {
+    inc: undefined,
+    size: 25,
+    padding: 10,
+    style: {
+        line: { color: '#212121', width: 2 },
+        ring: {
+            'stroke-dasharray': '1,2',
+            fill: 'transparent',
+            stroke: '#212121'
+        },
+        node: {
+            bg: '#212121',
+            fg: '#FFFFFF',
+            anim: {
+                scale: 1.5,
+                textScale: .5
+            }
+        }
+    }
+};
+let radial_config = {
+    container_id: undefined,
+    tree: undefined,
+    clickListener: undefined
+};
+let points = [];
+
+let utils = {
+    extractNumber: function ( str ) {
+        return str.match( /[0-9]+\.*[0-9]*/g )[ 0 ];
+    },
+    algebra: {
+        prodPunto: function ( a, b ) {
+            return (a.x * b.x) + (a.y * b.y);
+        },
+        normalVector: function ( a ) {
+            let c1 = a.x;
+            let c2 = a.y;
+            return Math.sqrt( Math.pow( c1, 2 ) + Math.pow( c2, 2 ) );
+        },
+        angulo: function ( a, b ) {
+
+            let angulo = Math.atan( (b.y - a.y) / (b.x - a.x) );
+
+            if ( b.x <= a.x ) {
+                if ( b.y >= a.y ) {
+                    return Math.PI + Math.abs( angulo );
+                }
+                else {
+                    return Math.PI - Math.abs( angulo );
+                }
+            }
+            else {
+                if ( b.y >= a.y ) {
+                    return (2 * Math.PI) - Math.abs( angulo );
+                }
+                else {
+                    return Math.abs( angulo );
+                }
+            }
+        },
+        anguloLR: function ( L, R ) {
+            if ( R === 0 ) {
+                return 0;
+            }
+            return L / R;
+        },
+        getComps: function ( R, a ) {
+            return [ R * Math.cos( a ), R * Math.sin( a ) ];
+        },
+        getDist: function ( a, b ) {
+            return Math.sqrt( Math.pow( Math.abs( a.x - b.x ), 2 ) + Math.pow( Math.abs( a.y - b.y ), 2 ) );
+        }
+    }
+};
+
+SVG.on( document, 'DOMContentLoaded', function () {
+    restart();
+} );
+
+function restart() {
+    let draw = SVG( radial_config.container_id ).size( '100%', '100%' );
+    draw.style( { padding: (10 + radial_attrs.size) + 'px' } );
+
+    let style = window.getComputedStyle( document.getElementById( draw.node.id ), null );
+    let height = utils.extractNumber( style.height );
+    let width = utils.extractNumber( style.width );
+    let center = { x: width / 2, y: height / 2 };
+
+    function Point( x, y, rad, nodo ) {
+        this.x = x;
+        this.y = y;
+        this.rad = rad;
+        this.nodo = nodo;
+        this.draw = function () {
+            let self = this;
+            let grupo = draw.group();
+            grupo.style( 'cursor', 'pointer' );
+
+            let circle = draw.circle( this.rad * 2 );
+            circle.center( this.x, this.y );
+            circle.attr( { fill: ((nodo.element.style && nodo.element.style.bg) || radial_attrs.style.node.bg) } );
+            grupo.add( circle );
+
+            let text = undefined;
+            if ( this.nodo.element.display && (this.nodo.element.display.normal || this.nodo.element.display.normal_svg) ) {
+                if ( this.nodo.element.display.normal ) {
+                    text = draw.text( this.nodo.element.display.normal.toString() );
+                    text.center( this.x, this.y );
+                    text.style( { fill: ((nodo.element.style && nodo.element.style.fg) || radial_attrs.style.node.fg) } );
+
+                    grupo.add( text );
+                }
+                else {
+                    let g = draw.group();
+                    g.svg( this.nodo.element.display.normal_svg );
+                    g.center( this.x, this.y );
+                    grupo.add( g );
+                }
+            }
+
+            let emptyCircle = draw.circle( this.rad * 2 );
+            emptyCircle.center( this.x, this.y );
+            emptyCircle.attr( { fill: 'transparent' } );
+            grupo.add( emptyCircle );
+
+            grupo.click( function () {
+                if ( radial_config.clickListener ) {
+                    radial_config.clickListener( nodo.element );
+                }
+            } );
+            grupo.mouseover( function () {
+                grupo.animate( { duration: 60 } ).scale( radial_attrs.style.node.anim.scale );
+                if ( text ) {
+                    text.scale( radial_attrs.style.node.anim.textScale );
+                    text.text( self.nodo.element.display.hover );
+                    text.center( self.x, self.y );
+                }
+            } );
+            grupo.mouseout( function () {
+                grupo.animate( { duration: 60 } ).scale( 1 );
+                if ( text ) {
+                    text.scale( 1 );
+                    text.text( self.nodo.element.display.normal.toString() );
+                    text.center( self.x, self.y );
+                }
+            } );
+        };
+    }
+
+    (function paintRings() {
+        function paintRing( rad ) {
+            let ring = draw.circle( rad * 2 );
+            ring.attr( radial_attrs.style.ring );
+            ring.center( center.x, center.y );
+        }
+
+        let treeHeight = radial_config.tree.height();
+        if ( !radial_attrs.inc ) {
+            radial_attrs.inc = height / (treeHeight - 1);
+            radial_attrs.inc /= 2;
+        }
+        for ( let i = treeHeight - 1; i > 0; i-- ) {
+            paintRing( i * radial_attrs.inc );
+        }
+    })();
+    (function paint( nodo, angulo, radio ) {
+        let childPoints = [];
+        let actualAngulo = angulo;
+
+        if ( nodo.childs && nodo.childs.length !== 0 ) {
+            nodo.childs.forEach( function ( child ) {
+                let point = paint( child, actualAngulo, radio + radial_attrs.inc );
+                actualAngulo = point.angulo;
+
+                if ( !child.element.hide ) {
+                    childPoints.push( point.pos );
+                }
+            } );
+            if ( nodo.childs.length === 1 ) {
+                actualAngulo += utils.algebra.anguloLR( radial_attrs.padding * 2, radio );
+            }
+        }
+        else {
+            actualAngulo += utils.algebra.anguloLR( (radial_attrs.size * 2) + radial_attrs.padding, radio );
+        }
+
+        let comp = utils.algebra.getComps( radio, angulo );
+        if ( nodo.childs.length > 1 ) {
+            comp = utils.algebra.getComps( radio, angulo + ((actualAngulo - angulo) / 2) - (utils.algebra.anguloLR( radial_attrs.size, radio )) );
+        }
+
+        if ( !nodo.element.hide ) {
+            childPoints.forEach( function ( point ) {
+                let origenX = center.x + comp[ 0 ];
+                let origenY = center.y - comp[ 1 ];
+                let destinoX = center.x + point[ 0 ];
+                let destinoY = center.y - point[ 1 ];
+
+                let a = utils.algebra.angulo( { x: origenX, y: origenY }, { x: destinoX, y: destinoY } );
+                let deltaY0 = radial_attrs.size * Math.sin( a );
+                let deltaX0 = radial_attrs.size * Math.cos( a );
+
+                origenX += deltaX0;
+                origenY -= deltaY0;
+                destinoX -= deltaX0;
+                destinoY += deltaY0;
+
+                let line = draw.line( origenX, origenY, destinoX, destinoY );
+                line.stroke( radial_attrs.style.line );
+            } );
+
+            let p = new Point( center.x + comp[ 0 ], center.y - comp[ 1 ], radial_attrs.size, nodo );
+            p.draw();
+            points.push( p );
+        }
+        return { angulo: actualAngulo, pos: comp };
+    })( radial_config.tree.root, 0, 0 );
+}
